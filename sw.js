66a49e96a80ad08849b3cb504f26ba870dd49579
alonzozaldivar <149989063+alonzozaldivar@users.noreply.github.com>
2026-06-24 21:49:41 -0700

    Update cache version from v37 to v38

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 📑 CACHÉ OFICIAL V30 - Sincronizado para limpiar errores de referencia previos
-const CACHE_NAME = 'fifa-united-2026-v37';
+const CACHE_NAME = 'fifa-united-2026-v38';
 const ASSETS = [
   './index.html',
   './manifest.json'

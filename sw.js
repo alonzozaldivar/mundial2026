b5a9f7fde34a39cff7c7bfb3ae1818f85d029406
alonzozaldivar <149989063+alonzozaldivar@users.noreply.github.com>
2026-06-24 23:54:29 -0700

    Update CACHE_NAME to version 44 in sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 📑 CACHÉ OFICIAL V30 - Sincronizado para limpiar errores de referencia previos
-const CACHE_NAME = 'fifa-united-2026-v43';
+const CACHE_NAME = 'fifa-united-2026-v44';
 const ASSETS = [
   './index.html',
   './manifest.json'
